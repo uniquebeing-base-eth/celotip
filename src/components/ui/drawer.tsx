ac465e2a0566fb3106f08193694dad9be55d7be5
Uniquebeing <beingunique049@gmail.com>
2025-12-10 15:57:35 +0100

    Update drawer.tsx

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -1,18 +1,24 @@
 import * as React from "react";
 import { Drawer as DrawerPrimitive } from "vaul";
 
+
 import { cn } from "@/lib/utils";
+
 
 const Drawer = ({ shouldScaleBackground = true, ...props }: React.ComponentProps<typeof DrawerPrimitive.Root>) => (
   <DrawerPrimitive.Root shouldScaleBackground={shouldScaleBackground} {...props} />
 );
 Drawer.displayName = "Drawer";
 
+
 const DrawerTrigger = DrawerPrimitive.Trigger;
+
 
 const DrawerPortal = DrawerPrimitive.Portal;
 
+
 const DrawerClose = DrawerPrimitive.Close;
+
 
 const DrawerOverlay = React.forwardRef<
   React.ElementRef<typeof DrawerPrimitive.Overlay>,
