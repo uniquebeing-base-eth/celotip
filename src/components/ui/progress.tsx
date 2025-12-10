86bf8e1eae78a237b0797a5d1afff83fddbb659b
Uniquebeing <beingunique049@gmail.com>
2025-12-10 16:03:14 +0100

    Update progress.tsx

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
 
+
 import { cn } from "@/lib/utils";
+
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
@@ -19,5 +21,6 @@ const Progress = React.forwardRef<
   </ProgressPrimitive.Root>
 ));
 Progress.displayName = ProgressPrimitive.Root.displayName;
+
 
 export { Progress };
