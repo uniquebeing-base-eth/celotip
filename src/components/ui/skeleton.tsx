fad82e2b23ce592115e5bb4133649cc44a3583b6
Uniquebeing <beingunique049@gmail.com>
2025-12-10 16:31:01 +0100

    Update skeleton.tsx

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -4,4 +4,5 @@ function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>)
   return <div className={cn("animate-pulse rounded-md bg-muted", className)} {...props} />;
 }
 
+
 export { Skeleton };
