5ac280fea207ef9657be223158bd4b891ece8082
Uniquebeing <beingunique049@gmail.com>
2025-12-10 16:30:36 +0100

    Update slider.tsx

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import * as SliderPrimitive from "@radix-ui/react-slider";
 
+
 import { cn } from "@/lib/utils";
+
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
@@ -20,4 +22,6 @@ const Slider = React.forwardRef<
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
 
+
 export { Slider };
+
