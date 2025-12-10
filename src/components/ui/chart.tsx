2c80df00ce2e465f67e4b4405177b5a90848c80c
Uniquebeing <beingunique049@gmail.com>
2025-12-10 15:53:43 +0100

    Update chart.tsx

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import * as RechartsPrimitive from "recharts";
 
+
 import { cn } from "@/lib/utils";
+
 
 // Format: { THEME_NAME: CSS_SELECTOR }
 const THEMES = { light: "", dark: ".dark" } as const;
+
 
 export type ChartConfig = {
   [k in string]: {
@@ -13,9 +16,11 @@ export type ChartConfig = {
   } & ({ color?: string; theme?: never } | { color?: never; theme: Record<keyof typeof THEMES, string> });
 };
 
+
 type ChartContextProps = {
   config: ChartConfig;
 };
+
 
 const ChartContext = React.createContext<ChartContextProps | null>(null);
 
