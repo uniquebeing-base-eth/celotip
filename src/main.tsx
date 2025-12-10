76284799afb91555f1f61837ee397d2374e0c1c6
Uniquebeing <beingunique049@gmail.com>
2025-12-10 17:24:31 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import App from "./App.tsx";
 import "./index.css";
 import { initializeFarcasterSDK } from "./lib/farcaster";
 
+
 // Initialize Farcaster SDK when app loads
 initializeFarcasterSDK().catch(console.error);
+
 
 createRoot(document.getElementById("root")!).render(<App />);
