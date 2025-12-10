f5db4c2d127be7b1423b2712f81b3dac03628432
Uniquebeing <beingunique049@gmail.com>
2025-12-10 16:29:29 +0100

    Update switch.tsx

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -3,6 +3,7 @@ import * as SwitchPrimitives from "@radix-ui/react-switch";
 
 import { cn } from "@/lib/utils";
 
+
 const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
   React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
@@ -24,4 +25,7 @@ const Switch = React.forwardRef<
 ));
 Switch.displayName = SwitchPrimitives.Root.displayName;
 
+
 export { Switch };
+
+
