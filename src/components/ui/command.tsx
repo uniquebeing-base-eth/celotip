1cee267ff729b3537080f90769f465d444f613cc
Uniquebeing <beingunique049@gmail.com>
2025-12-10 15:55:17 +0100

    Update command.tsx

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -3,8 +3,10 @@ import { type DialogProps } from "@radix-ui/react-dialog";
 import { Command as CommandPrimitive } from "cmdk";
 import { Search } from "lucide-react";
 
+
 import { cn } from "@/lib/utils";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
+
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
@@ -21,7 +23,9 @@ const Command = React.forwardRef<
 ));
 Command.displayName = CommandPrimitive.displayName;
 
+
 interface CommandDialogProps extends DialogProps {}
+
 
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
@@ -34,6 +38,7 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
     </Dialog>
   );
 };
+
 
 const CommandInput = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
