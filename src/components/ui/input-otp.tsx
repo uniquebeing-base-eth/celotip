3b5672769cb7809b97e2cd5ab736cdabe11b22a1
Uniquebeing <beingunique049@gmail.com>
2025-12-10 15:59:50 +0100

    Update input-otp.tsx

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import { OTPInput, OTPInputContext } from "input-otp";
 import { Dot } from "lucide-react";
 
+
 import { cn } from "@/lib/utils";
+
 
 const InputOTP = React.forwardRef<React.ElementRef<typeof OTPInput>, React.ComponentPropsWithoutRef<typeof OTPInput>>(
   ({ className, containerClassName, ...props }, ref) => (
@@ -16,10 +18,12 @@ const InputOTP = React.forwardRef<React.ElementRef<typeof OTPInput>, React.Compo
 );
 InputOTP.displayName = "InputOTP";
 
+
 const InputOTPGroup = React.forwardRef<React.ElementRef<"div">, React.ComponentPropsWithoutRef<"div">>(
   ({ className, ...props }, ref) => <div ref={ref} className={cn("flex items-center", className)} {...props} />,
 );
 InputOTPGroup.displayName = "InputOTPGroup";
+
 
 const InputOTPSlot = React.forwardRef<
   React.ElementRef<"div">,
