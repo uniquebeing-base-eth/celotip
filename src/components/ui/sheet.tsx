33b9865fea251211cde67fadffbc8b414460a1b5
Uniquebeing <beingunique049@gmail.com>
2025-12-10 16:34:15 +0100

    Update sheet.tsx

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -3,15 +3,21 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { X } from "lucide-react";
 import * as React from "react";
 
+
 import { cn } from "@/lib/utils";
+
 
 const Sheet = SheetPrimitive.Root;
 
+
 const SheetTrigger = SheetPrimitive.Trigger;
+
 
 const SheetClose = SheetPrimitive.Close;
 
+
 const SheetPortal = SheetPrimitive.Portal;
+
 
 const SheetOverlay = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Overlay>,
@@ -27,6 +33,7 @@ const SheetOverlay = React.forwardRef<
   />
 ));
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
+
 
 const sheetVariants = cva(
   "fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
@@ -46,6 +53,7 @@ const sheetVariants = cva(
     },
   },
 );
+
 
 interface SheetContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
