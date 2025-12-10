dcec98c07511db8f34bbac0fdec0ce24d98ca4cf
Uniquebeing <beingunique049@gmail.com>
2025-12-10 15:56:54 +0100

    Update dialog.tsx

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -2,15 +2,21 @@ import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
 
+
 import { cn } from "@/lib/utils";
+
 
 const Dialog = DialogPrimitive.Root;
 
+
 const DialogTrigger = DialogPrimitive.Trigger;
+
 
 const DialogPortal = DialogPrimitive.Portal;
 
+
 const DialogClose = DialogPrimitive.Close;
+
 
 const DialogOverlay = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Overlay>,
@@ -26,6 +32,7 @@ const DialogOverlay = React.forwardRef<
   />
 ));
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
+
 
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
