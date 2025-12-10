7d3939a6ce686408035fc4a60365aaf0bee27b78
Uniquebeing <beingunique049@gmail.com>
2025-12-10 16:35:43 +0100

    Update checkbox.tsx

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { Check } from "lucide-react";
 
+
 import { cn } from "@/lib/utils";
+
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
@@ -22,5 +24,6 @@ const Checkbox = React.forwardRef<
   </CheckboxPrimitive.Root>
 ));
 Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
 
 export { Checkbox };
